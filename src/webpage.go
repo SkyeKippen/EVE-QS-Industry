992dabb7e8a01fd0fbbe43b3fccb0319841228fc
SkyeMeadows <skyemeadows20@gmail.com>
2026-07-23 13:34:16 -0500

    fixed logging miscommunication

--- src/webpage.go
+++ src/webpage.go
@@ -286,7 +286,7 @@ func renderSubmitOrderChanges(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Println("Modified order with the values:", orderItem, orderQuantity, orderPrice, orderLocation, orderContractTo, "by", sess.CharacterName)
+	log.Println("Modified order with the values:", orderInternalId, orderQuantity, orderPrice, orderLocation, orderContractTo, "by", sess.CharacterName)
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tmpl.ExecuteTemplate(w, "manage_order.html", nil)
