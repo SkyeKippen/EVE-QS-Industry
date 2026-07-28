65415e21a9e71ca445c9dc3198c3a2863cd23943
SkyeMeadows <skyemeadows20@gmail.com>
2026-07-28 05:07:04 -0500

    Fixed Extra fields

--- src/db/OrderDbHandler.go
+++ src/db/OrderDbHandler.go
@@ -29,6 +29,7 @@ type Order struct {
 	ContractTo        string  `json:"contractTo"`
 	CreatedBy         string  `json:"createdBy"`
 	Fulfilled         bool    `json:"fulfilled"`
+	Denied            bool    `json:"denied"`
 }
 
 var (
@@ -98,7 +99,7 @@ func LoadAllIndustryOrders() ([]Order, error) {
 	var allOrders []Order
 	for rows.Next() {
 		var order Order
-		err = rows.Scan(&order.InternalIdCounter, &order.TypeId, &order.Quantity, &order.Price, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled)
+		err = rows.Scan(&order.InternalIdCounter, &order.TypeId, &order.Quantity, &order.Price, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled, &order.Denied)
 		if err != nil {
 			return nil, err
 		}
@@ -203,7 +204,7 @@ func FetchOrderById(orderId int) (Order, error) {
 		FROM meadow_works.industry_orders
 		WHERE internal_order_id = $1`,
 		orderId,
-	).Scan(&order.InternalIdCounter, &order.TypeId, &order.Price, &order.Quantity, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled)
+	).Scan(&order.InternalIdCounter, &order.TypeId, &order.Price, &order.Quantity, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled, &order.Denied)
 
 	if err != nil {
 		return Order{}, err
@@ -248,7 +249,7 @@ func LoadUserOrders(sess *auth.Session) ([]Order, error) {
 	var userOrders []Order
 	for rows.Next() {
 		var order Order
-		err = rows.Scan(&order.InternalIdCounter, &order.TypeId, &order.Quantity, &order.Price, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled)
+		err = rows.Scan(&order.InternalIdCounter, &order.TypeId, &order.Quantity, &order.Price, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled, &order.Denied)
 		if err != nil {
 			return nil, err
 		}
@@ -326,7 +327,7 @@ func LoadUserFulfilledOrders(sess *auth.Session) ([]Order, error) {
 	var userFulfilledOrders []Order
 	for rows.Next() {
 		var order Order
-		err = rows.Scan(&order.InternalIdCounter, &order.TypeId, &order.Quantity, &order.Price, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled)
+		err = rows.Scan(&order.InternalIdCounter, &order.TypeId, &order.Quantity, &order.Price, &order.Location, &order.ContractTo, &order.CreatedBy, &order.Fulfilled, &order.Denied)
 		if err != nil {
 			return nil, err
 		}
